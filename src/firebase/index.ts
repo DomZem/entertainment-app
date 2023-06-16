154d3b481dcc9c7e2b6ff695e2a756ed551cc9fc
Dominik <dominik.zemlik.dev@gmail.com>
2023-06-16 23:45:39 +0200

    Configure project

---
+++ src/firebase/index.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
+  authDomain: 'entertainment-app-606e1.firebaseapp.com',
+  projectId: 'entertainment-app-606e1',
+  storageBucket: 'entertainment-app-606e1.appspot.com',
+  messagingSenderId: '439631894272',
+  appId: '1:439631894272:web:9653726eab8d14bd2b85e5',
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = initializeApp(firebaseConfig);
